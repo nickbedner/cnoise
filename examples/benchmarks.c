2eebe9d8ff6aa110454f0a75ec2a4f3e025a1449
Zalrioth <nicholasbedner@gmail.com>
2019-11-23 00:39:41 -0800

    Travis fix

--- examples/benchmarks.c
+++ examples/benchmarks.c
@@ -24,14 +24,11 @@ int main(int argc, char* argv[]) {
 }
 
 static inline float run_benchmark(float* (*perlin_func)(struct PerlinNoise*, size_t, size_t, size_t), struct PerlinNoise* perlin_noise, size_t size_x, size_t size_y, size_t size_z, bool parallel) {
-  float start_time, end_time;
-  float* data;
-
   perlin_noise->parallel = parallel;
 
-  start_time = (float)clock() / CLOCKS_PER_SEC;
-  data = perlin_func(perlin_noise, size_x, size_y, size_z);
-  end_time = (float)clock() / CLOCKS_PER_SEC;
+  float start_time = (float)clock() / CLOCKS_PER_SEC;
+  float* data = perlin_func(perlin_noise, size_x, size_y, size_z);
+  float end_time = (float)clock() / CLOCKS_PER_SEC;
   noise_free(data);
 
   printf("///////////////////////////////////////////////////////////////////\n");
