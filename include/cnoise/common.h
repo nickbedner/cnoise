43b9deb29d9eae53f7143e56040706f15508be4b
Zalrioth <nicholasbedner@gmail.com>
2019-11-25 19:46:28 -0800

    Apple fix

--- include/cnoise/common.h
+++ include/cnoise/common.h
@@ -31,8 +31,8 @@
 #endif
 #endif
 
-#ifdef __GNUC__
-#if __GNUC__ < 8
+#if defined(__GNUC__) || defined(__APPLE__)
+#if __GNUC__ < 8 || defined(__APPLE__)
 #define _mm256_set_m128i(xmm1, xmm2) _mm256_permute2f128_si256(_mm256_castsi128_si256(xmm1), _mm256_castsi128_si256(xmm2), 2)
 #define _mm256_set_m128f(xmm1, xmm2) _mm256_permute2f128_ps(_mm256_castps128_ps256(xmm1), _mm256_castps128_ps256(xmm2), 2)
 #endif
