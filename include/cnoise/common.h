359bd5a1aa17e886190bdb66fd84357e5067f2b1
Zalrioth <nicholasbedner@gmail.com>
2019-11-25 16:20:16 -0800

    Boom

--- include/cnoise/common.h
+++ include/cnoise/common.h
@@ -36,7 +36,7 @@
 #define _mm256_set_m128i(xmm1, xmm2) _mm256_permute2f128_si256(_mm256_castsi128_si256(xmm1), _mm256_castsi128_si256(xmm2), 2)
 #define _mm256_set_m128f(xmm1, xmm2) _mm256_permute2f128_ps(_mm256_castps128_ps256(xmm1), _mm256_castps128_ps256(xmm2), 2)
 #endif
-#endi
+#endif
 
 enum NoiseQuality {
   QUALITY_FAST,
