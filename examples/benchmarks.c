665455687ec484451ca760601c43778b075da8bf
Zalrioth <nicholasbedner@gmail.com>
2019-12-03 21:55:21 -0800

    Test

--- examples/benchmarks.c
+++ examples/benchmarks.c
@@ -5,10 +5,10 @@
 
 static inline float run_benchmark(float* (*perlin_func)(struct PerlinNoise*, size_t, size_t, size_t), struct PerlinNoise* perlin_noise, size_t size_x, size_t size_y, size_t size_z, bool parallel);
 
-#undef _mm_extract_epi32
-#define _mm_extract_epi32(v, n) *(((int32_t*)&v) + n)
-#undef _mm256_set_m128i
-#define _mm256_set_m128i(xmm1, xmm2) _mm256_set_epi32(_mm_extract_epi32(xmm1, 3), _mm_extract_epi32(xmm1, 2), _mm_extract_epi32(xmm1, 1), _mm_extract_epi32(xmm1, 0), _mm_extract_epi32(xmm2, 3), _mm_extract_epi32(xmm2, 2), _mm_extract_epi32(xmm2, 1), _mm_extract_epi32(xmm2, 0))
+//#undef _mm_extract_epi32
+//#define _mm_extract_epi32(v, n) *(((int32_t*)&v) + n)
+//#undef _mm256_set_m128i
+//#define _mm256_set_m128i(xmm1, xmm2) _mm256_set_epi32(_mm_extract_epi32(xmm1, 3), _mm_extract_epi32(xmm1, 2), _mm_extract_epi32(xmm1, 1), _mm_extract_epi32(xmm1, 0), _mm_extract_epi32(xmm2, 3), _mm_extract_epi32(xmm2, 2), _mm_extract_epi32(xmm2, 1), _mm_extract_epi32(xmm2, 0))
 
 int main(int argc, char* argv[]) {
   const int size_x = 8, size_y = 1, size_z = 1;
@@ -34,8 +34,8 @@ int main(int argc, char* argv[]) {
   //__m128i x1_high = _mm256_extractf128_si256(x0, 1);
   //x1_high = _mm_add_epi32(x1_high, _mm_set1_epi32(1));
   // TODO: Figure out what instruction causes problem here on osx
-  __m256i x1 = _mm256_set_m128i(x1_low, x1_low);
-  printf("Test: %d", x1);
+  //__m256i x1 = _mm256_set_m128i(x1_low, x1_low);
+  printf("Test: %d", x1_low);
   //printf("Test: %d %d", x1_low, x1_high);
 
 //  if (check_simd_support(SIMD_AVX2)) {
